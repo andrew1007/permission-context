4141d4d893e97ec7472fb562c5c8556a8b12ced3
andrew <yueh.andrew@gmail.com>
2021-09-06 04:02:02 -0700

    better bool checking in hook

--- src/useHasFeature/useHasFeature.ts
+++ src/useHasFeature/useHasFeature.ts
@@ -5,6 +5,6 @@ import { FFContext } from "./FeatureFlagProvider"
 export default function useHasFeature() {
     const flags = useContext(FFContext)
     return {
-        hasFeature: (flag: string) => flags[flag] ?? false
+        hasFeature: (flag: string) => flag in flags
     }
 }
